0118954239c42b2c78e2a1e1dce747a134ca883b
shamael203 <shamael2003@gmail.com>
2025-12-16 23:17:25 +0300

    sql

--- sql/book_exchange (20).sql
+++ sql/book_exchange (20).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- مضيف: localhost
--- وقت الجيل: 14 ديسمبر 2025 الساعة 01:07
+-- وقت الجيل: 16 ديسمبر 2025 الساعة 20:16
 -- إصدار الخادم: 8.0.43
 -- نسخة PHP: 8.2.29
 
@@ -44,11 +44,9 @@ CREATE TABLE `books` (
 
 INSERT INTO `books` (`edition`, `price`, `id`, `title`, `author`, `user_id`, `image`, `category`) VALUES
 ('الطبعه الثانية', 99, 19, 'عصر التنقيب في البيانات عبر تقنيات الذكاء الاصطناعي التطور – التحديات الفنية والقانونية – العيوب خوارزمية الذكاء الاصطناعي دراسة مقارنة', ' احمد حسين علي', 14, '1765048450_3ea13b05-6e1a-4de8-a80e-1c3849436b98-thumbnail-1000x1000-70.jpg', NULL),
-('الطبعة الأولى', 99, 20, 'عصر التنقيب في البيانات', 'أحمد حسين علي', 1, 'ai.jpg', 'تقنية'),
-('الطبعه الثانية', 77, 21, 'علوم الحاسب الالي و المعلومات المدخل إلى البرمجة الخطية وتطبيقاتها في الإدارة', 'خالد موسى الطاسان', 14, '1765230460_1444.png', NULL),
-('الطبعه الثانية', 77, 22, 'l', 'خالد موسى الطاسان', 14, '1765234849_1444.png', NULL),
-('الرابعة', 77, 23, 'المهارات اللغوية عرب 101', 'خالد محمد', 19, '1765670488_47b1ca32-5389-41bb-ba13-1051c2ad40c3-1000x1000-d47NWMD2DVegn8rOwTZltjzCJM9SFJU5G2P92efw.webp', NULL),
-('الرابعة', 99, 24, 'المهارات اللغوية', 'احمد علي', 14, '1765674099_47b1ca32-5389-41bb-ba13-1051c2ad40c3-1000x1000-d47NWMD2DVegn8rOwTZltjzCJM9SFJU5G2P92efw.webp', NULL);
+('2', 60, 31, 'effective academic writing intro', 'Philip Kotler, Gary Armstrong', 19, '1765680671_7EYNyxGELBAq0CWyAdCx3ftZO5lbjrSyPmky1IYn.webp', NULL),
+('2', 90, 32, 'english vocabulary in usa 3rd edition', 'Philip Kotler', 19, '1765680725_t3IX9XGjpPz7i71NylCMFbzG6kbO8AT56EAHGb7Y.webp', NULL),
+('1', 65, 33, 'الاجراءات الجزئية', 'د.فهد الطريسي', 20, '1765914525_a2dc1cc1-32b3-478b-86ee-b17a95e9c3ad.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,16 +60,6 @@ CREATE TABLE `cart` (
   `book_id` int NOT NULL,
   `quantity` int NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- إرجاع أو استيراد بيانات الجدول `cart`
---
-
-INSERT INTO `cart` (`id`, `user_id`, `book_id`, `quantity`) VALUES
-(9, 1, 20, 1),
-(13, 14, 20, 1),
-(14, 19, 23, 1),
-(15, 14, 21, 2);
 
 -- --------------------------------------------------------
 
@@ -94,23 +82,11 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `sender_id`, `receiver_id`, `message`, `date`, `book_id`, `seen`) VALUES
-(1, 14, 14, 'gt', '2025-12-14 03:49:05', 19, 1),
-(2, 14, 14, 'www', '2025-12-14 03:49:05', 19, 1),
-(3, 14, 14, 'z', '2025-12-14 03:49:05', 19, 1),
-(4, 14, 14, 'ي', '2025-12-14 03:49:05', 19, 1),
-(5, 14, 14, 'السلام عليكم ', '2025-12-14 03:49:05', 19, 1),
-(6, 14, 14, 'كيف حالك', '2025-12-14 03:49:05', 19, 1),
-(7, 14, 14, 'ا', '2025-12-14 03:49:05', 19, 1),
-(8, 14, 14, 'ت', '2025-12-14 03:49:05', 19, 1),
-(9, 14, 14, 'ت', '2025-12-14 03:49:05', 19, 1),
-(10, 14, 1, 'ا ى', '2025-12-14 02:40:36', NULL, 0),
-(11, 19, 14, 's', '2025-12-14 03:24:33', 19, 1),
-(12, 19, 14, 's', '2025-12-14 03:24:33', 19, 1),
-(13, 14, 19, 'ا', '2025-12-14 03:13:53', 23, 1),
-(14, 19, 14, 'السلام عليكم ', '2025-12-14 03:24:33', 19, 1),
-(15, 19, 14, 'السلام عليكم ', '2025-12-14 03:24:33', 19, 1),
-(16, 14, 19, 'يرءي', '2025-12-14 03:24:39', 23, 0),
-(17, 14, 19, 'ب', '2025-12-14 03:24:43', 23, 0);
+(29, 14, 19, 'السلام عليكم ', '2025-12-15 06:17:24', NULL, 1),
+(32, 19, 14, 'وعليكم السلام', '2025-12-15 06:50:08', NULL, 1),
+(33, 14, 19, 'وعليكم السلام', '2025-12-15 07:14:39', NULL, 0),
+(34, 20, 14, 'السلام عليكم', '2025-12-16 22:43:47', NULL, 1),
+(35, 14, 20, 'وعليكم السلام', '2025-12-16 22:47:46', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -139,7 +115,10 @@ INSERT INTO `orders` (`id`, `user_id`, `name`, `email`, `address`, `payment_meth
 (3, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 113.85, '2025-12-06 21:51:18'),
 (4, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 113.85, '2025-12-06 21:53:34'),
 (5, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 113.85, '2025-12-06 22:17:57'),
-(6, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 113.85, '2025-12-13 22:05:08');
+(6, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 113.85, '2025-12-13 22:05:08'),
+(7, 19, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'card', 0.00, '2025-12-14 02:50:35'),
+(8, 14, 'shmael sultan', 'shamael1423@gmail.com', 'المدينة المنوره ', 'cash', 69.00, '2025-12-15 04:26:41'),
+(9, 20, 'Arwa', 'Arwa12@gmail.cim', 'المدينة المنوره ', 'cash', 113.85, '2025-12-16 19:41:34');
 
 -- --------------------------------------------------------
 
@@ -162,7 +141,9 @@ CREATE TABLE `order_items` (
 INSERT INTO `order_items` (`id`, `order_id`, `book_id`, `quantity`, `price`) VALUES
 (1, 4, 19, 1, 99.00),
 (2, 5, 19, 1, 99.00),
-(3, 6, 19, 1, 99.00);
+(3, 6, 19, 1, 99.00),
+(4, 8, 31, 1, 60.00),
+(5, 9, 19, 1, 99.00);
 
 -- --------------------------------------------------------
 
@@ -201,7 +182,8 @@ CREATE TABLE `profile` (
 --
 
 INSERT INTO `profile` (`id`, `user_id`, `bio`, `phone`, `avatar`, `join_date`, `location`) VALUES
-(1, 14, '', '0536910118', 'uploads/avatar_14_1765664903.png', '2025-12-14 00:24:38', NULL);
+(1, 14, '', '0536910118', 'uploads/avatar_14_1765664903.png', '2025-12-14 00:24:38', NULL),
+(2, 19, '', '', 'avatars/avatar_19_1765678651.webp', '2025-12-14 05:10:29', NULL);
 
 -- --------------------------------------------------------
 
@@ -224,7 +206,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`password`, `created_at`, `id`, `name`, `email`) VALUES
 ('$2y$10$zg58cT9rCZ.eb2QA8RrpY.r0KuJlLO.PX4fqRpl19b.UFxpsu5yXS', '2025-10-08 22:58:23', 1, 'Shamael sultan', 'shamaelm2003@gmail.com'),
 ('$2y$10$mOnZv9ur7MaZf4vkeGMM0.8CD7oTQRZXllybRVQDertIZ.UGG.R4q', '2025-10-14 08:49:22', 14, 'Shamael sultan', 'shamael1423@gmail.com'),
-('$2y$10$sZPM9VPvAS1Q8DadqBi6suZBlVI3dwQ0546XoLRSCgjkhq5FXbLJK', '2025-12-13 23:53:51', 19, 'ammar', 'amaaar.1427@gmail.com');
+('$2y$10$sZPM9VPvAS1Q8DadqBi6suZBlVI3dwQ0546XoLRSCgjkhq5FXbLJK', '2025-12-13 23:53:51', 19, 'ammar', 'amaaar.1427@gmail.com'),
+('$2y$10$Z6mbOegJ2CivOwgDvR6WW.bUf9YXTu41Q84zZS5FKYo.UG7gMwP/m', '2025-12-16 19:40:03', 20, 'Arwa', 'Arwa12@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -296,31 +279,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `payments`
@@ -332,13 +315,13 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- القيود المفروضة على الجداول الملقاة
